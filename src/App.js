cfe4174a6d1ebe70e8b27b0a4e1792a719b0ebda
Julio Santacruz <aram.jsh1@gmail.com>
2022-04-05 00:18:49 -0700

    aqui configuramos el useState y use Effect para la aplicacion To Do.!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { TodoSearch } from './components/TodoSearch';
 import { CreateTodoButton } from './components/CreateTodoButton';
 import { TodoList } from './components/TodoList';
 import { TodoItem } from './components/TodoItem';
-// import './App.css';
+
 
 // const todosDefault = [
 //   { text: 'Crear tu primer To Do..', completed: false },
@@ -13,45 +13,83 @@ import { TodoItem } from './components/TodoItem';
 //   { text: 'Comer frutas y verduras', completed: false }]
 
 
-function useLocalStorage(itemName, initialValue){
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
+function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false)
+  const [loading, setLoading] = React.useState(true)
+  const [item, setItem] = React.useState(initialValue)
 
-  if(!localStorageItem){
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  }else{
-    parsedItem = JSON.parse(localStorageItem)
-  }
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
 
-  const [item, setItem] = React.useState(parsedItem)
-    
-  
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem)
+        }
+
+        setItem(parsedItem)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+      }
+    }, 3000)
+  })
+
+
   //Aqui va la logica para conectar localStorage al save / delete
-  const saveItem = (newItem) =>{
-    const stringifiedItem = JSON.stringify(newItem)
-    localStorage.setItem(itemName, stringifiedItem)
-    setItem(newItem)
-  } 
-  return [
-    item, saveItem
-  ]
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem)
+      localStorage.setItem(itemName, stringifiedItem)
+      setItem(newItem)
+    } catch (error) {
+      setError(error)
+    }
+  }
+  return {
+    item,
+    saveItem,
+    loading,
+    error
+  }
 }
 
+
+
+
+
 function App() {
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', [])
+  
+
+  // console.log('Antes de useEfect')
+  // React.useEffect(() => {
+  //   console.log('Aqui esta el useEfect')
+  // }, [])
+  // console.log('despues del useEfect')
+
+
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading, 
+    error,
+  } = useLocalStorage('TODOS_V1', [])
   const [searchValue, setSearchValue] = React.useState('')
 
   //funcion para contar los elementos completados
-  const  completedTodos = todos.filter(todo => !!todo.completed).length;
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
-  
+
   //Aqui esta el codigo del buscador de To Do's
   let searchedTodos = []
 
-  if(!searchValue.length>=1){
+  if (!searchValue.length >= 1) {
     searchedTodos = todos;
-  }else{
+  } else {
     searchedTodos = todos.filter(todo => {
       const todoText = todo.text.toLowerCase()
       const searchText = searchValue.toLowerCase()
@@ -63,53 +101,57 @@ function App() {
 
 
   //Aqui va la logica para el boton de completado
-  const completeTodo = (text) =>{
+  const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text); //Identifica el index del elemento a completar
     const newTodos = [...todos] //Crea una nueva variable con todos los To Do's
-    if(newTodos[todoIndex].completed){ //Reviza si el item seleccionado es true
-      newTodos[todoIndex].completed=false
-    }else{
+    if (newTodos[todoIndex].completed) { //Reviza si el item seleccionado es true
+      newTodos[todoIndex].completed = false
+    } else {
       newTodos[todoIndex].completed = true // Cambia la variable completed a true
     }
     saveTodos(newTodos) //envio nuevi listado de todos
   }
 
-    //Aqui va la logica para el boton de completado
-    const deleteTodo = (text) =>{
-      const todoIndex = todos.findIndex(todo => todo.text === text); //Identifica el index del elemento a eliminar
-      const newTodos = [...todos] //Crea una nueva variable con todos los To Do's
-      newTodos.splice(todoIndex, 1)// Elimina el elemento
-      
-      saveTodos(newTodos) //envio nuevi listado de todos
-    }
+  //Aqui va la logica para el boton de completado
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text); //Identifica el index del elemento a eliminar
+    const newTodos = [...todos] //Crea una nueva variable con todos los To Do's
+    newTodos.splice(todoIndex, 1)// Elimina el elemento
+
+    saveTodos(newTodos) //envio nuevi listado de todos
+  }
 
 
   return (
     <React.Fragment>
-      <TodoCounter 
-        total = {totalTodos}
-        completedTodos = {completedTodos}
+      <TodoCounter
+        total={totalTodos}
+        completedTodos={completedTodos}
       />
-      
 
-      <TodoSearch 
-        searchValue ={searchValue}
-        setSearchValue = {setSearchValue}
-        />
-      
+
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+
       <TodoList >
+        {error && <p>Hay un error, reinicia</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {(!loading && !searchedTodos.length) && <h4>Crea tu primer todo..!</h4>}
+
         {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text} 
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
             completed={todo.completed}
-            onComplete ={()=> completeTodo(todo.text)}
-            onDelete= {()=> deleteTodo(todo.text)} />
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)} />
         ))}
 
       </TodoList>
       <CreateTodoButton />
-      
+
     </React.Fragment>
   );
 }
